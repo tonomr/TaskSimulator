14266190cd9a2ac7a6fe2f63d46321d4b6ea189c
tonomr <tonomac7@gmail.com>
2022-10-13 01:29:30 -0500

    all fifo program functions finished

--- fifo_processing.py
+++ fifo_processing.py
@@ -3,9 +3,11 @@ from random import randint
 from time import sleep
 
 from PySide6.QtCore import QThreadPool, QRunnable, QObject, Slot, Signal
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QProgressBar
+from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QGridLayout, QHBoxLayout,
+                               QVBoxLayout, QLabel, QPushButton, QProgressBar, QMessageBox)
 
 from task import Task
+
 
 class SignalProgress(QObject):
     """ Custom signals for our progress bars """
@@ -15,6 +17,7 @@ class SignalProgress(QObject):
 
 class RunProgress(QRunnable):
     """ Worker thread for the progress bars """
+
     def __init__(self, task, status, signal):
         super().__init__()
         self.task = task
@@ -26,12 +29,18 @@ class RunProgress(QRunnable):
     @Slot()
     def run(self):
         """ Run the animations """
+        # This checks if the other tasks did not start yet
+        if not self.is_finished:
+            self.status.setText('En Ejecucion')
+
         for s in range(self.task.execution_time + 1):
             # Send current progress of the bar
-            self.signal.progress.emit(int((self.task.elapsed_time * 100) / self.task.execution_time))
+            self.signal.progress.emit(
+                int((self.task.elapsed_time * 100) / self.task.execution_time))
             self.task.elapsed_time += 1
             sleep(1)
 
+            # Infine loop for pause
             while self.is_paused:
                 sleep(0)
 
@@ -40,22 +49,26 @@ class RunProgress(QRunnable):
 
             if s == self.task.execution_time - 1:
                 self.status.setText('Terminado')
-        
+
         self.is_finished = True
         self.signal.finished.emit()
         self.status.setText('Terminado')
-    
+
     def pause(self):
         """ Pause the progression of the bar """
         if not self.is_finished:
             self.is_paused = True
-            self.status.setText('Bloqueado')
+            # Same check for the start
+            if self.task.elapsed_time != 0:
+                self.status.setText('Bloqueado')
 
     def resume(self):
         """ Resume the progression of the bar """
         if not self.is_finished:
             self.is_paused = False
-            self.status.setText('En Ejecucion')
+            # Same check for the start
+            if self.task.elapsed_time != 0:
+                self.status.setText('En Ejecucion')
 
     def finish(self):
         """ Break the progression of the bar """
@@ -65,6 +78,7 @@ class RunProgress(QRunnable):
 
 class FIFOMainWindow(QMainWindow):
     """ Program that simulates FIFO algorithm for processing """
+
     def __init__(self):
         # Window configs
         super().__init__()
@@ -76,8 +90,8 @@ class FIFOMainWindow(QMainWindow):
         self.thread_pool = QThreadPool()
         self.thread_pool.setMaxThreadCount(5)
 
-        ### Left Widgets
-        ## We make agrupations for all the widgets to have better control, making a QGridLayout for each one
+        # Left Widgets
+        # We make agrupations for all the widgets to have better control, making a QGridLayout for each one
         # Proccess 01 Set
         container_p1 = QWidget()
         layout_p1 = QGridLayout(container_p1)
@@ -134,7 +148,7 @@ class FIFOMainWindow(QMainWindow):
         layout_p4.addWidget(self.label_time_p4, 0, 2)
         layout_p4.addWidget(self.progress_bar_p4, 1, 0, -1, -1)
 
-        ### Right Widgets
+        # Right Widgets
         # Buttons
         self.button_resume = QPushButton('Continuar')
         self.button_pause = QPushButton('Pausar')
@@ -142,11 +156,11 @@ class FIFOMainWindow(QMainWindow):
         self.button_assign = QPushButton('Asignar Procesos')
         self.button_execute = QPushButton('Ejecutar')
 
-        self.button_assign.clicked.connect(self.assign_tasks)
-        self.button_execute.clicked.connect(self.start_execution)
+        self.button_assign.clicked.connect(self.assign_tasks)  # type: ignore
+        self.button_execute.clicked.connect(self.start_execution)  # type: ignore
 
-        ### Main Layouts
-        ## This way we put all layouts and set the widgets on the window
+        # Main Layouts
+        # This way we put all layouts and set the widgets on the window
         layout_main = QHBoxLayout()
         layout_left = QVBoxLayout()
         layout_right = QGridLayout()
@@ -166,11 +180,11 @@ class FIFOMainWindow(QMainWindow):
         layout_right.addWidget(self.button_assign, 1, 0, 1, 1)
         layout_right.addWidget(self.button_execute, 1, 1, 1, 2)
 
-        container = QWidget()
-        container.setLayout(layout_main)
-        self.setCentralWidget(container)
+        self.container = QWidget()
+        self.container.setLayout(layout_main)
+        self.setCentralWidget(self.container)
     # __init__
-    
+
     def assign_tasks(self):
         """ Create tasks with random times, append to the list and update """
         # Clear the list for assign new tasks
@@ -181,7 +195,7 @@ class FIFOMainWindow(QMainWindow):
             time = randint(3, 15)
             task = Task(i+1, time, 0, time)
             self.task_list.append(task)
-        
+
         self.label_state_p1.setText('Listo')
         self.label_state_p2.setText('Listo')
         self.label_state_p3.setText('Listo')
@@ -205,66 +219,86 @@ class FIFOMainWindow(QMainWindow):
         """ Set all bars threads and signals """
         if self.task_list:
             self.signal_p1 = SignalProgress()
-            self.thread_p1 = RunProgress(self.task_list[0], self.label_state_p1, self.signal_p1)
+            self.thread_p1 = RunProgress(
+                self.task_list[0], self.label_state_p1, self.signal_p1)
             self.thread_p1.signal.progress.connect(self.update_progress_p1)
             self.thread_p1.signal.finished.connect(self.finish_p1)
 
             self.signal_p2 = SignalProgress()
-            self.thread_p2 = RunProgress(self.task_list[1], self.label_state_p2, self.signal_p2)
+            self.thread_p2 = RunProgress(
+                self.task_list[1], self.label_state_p2, self.signal_p2)
             self.thread_p2.signal.progress.connect(self.update_progress_p2)
             self.thread_p2.signal.finished.connect(self.finish_p2)
 
             self.signal_p3 = SignalProgress()
-            self.thread_p3 = RunProgress(self.task_list[2], self.label_state_p3, self.signal_p3)
+            self.thread_p3 = RunProgress(
+                self.task_list[2], self.label_state_p3, self.signal_p3)
             self.thread_p3.signal.progress.connect(self.update_progress_p3)
             self.thread_p3.signal.finished.connect(self.finish_p3)
 
             self.signal_p4 = SignalProgress()
-            self.thread_p4 = RunProgress(self.task_list[3], self.label_state_p4, self.signal_p4)
+            self.thread_p4 = RunProgress(
+                self.task_list[3], self.label_state_p4, self.signal_p4)
             self.thread_p4.signal.progress.connect(self.update_progress_p4)
             self.thread_p4.signal.finished.connect(self.finish_p4)
 
+            # Same signals for all tasks (1 button for all)
+            self.button_resume.pressed.connect(self.thread_p1.resume)  # type: ignore
+            self.button_resume.pressed.connect(self.thread_p2.resume)  # type: ignore
+            self.button_resume.pressed.connect(self.thread_p3.resume)  # type: ignore
+            self.button_resume.pressed.connect(self.thread_p4.resume)  # type: ignore
+
+            self.button_pause.pressed.connect(self.thread_p1.pause)  # type: ignore
+            self.button_pause.pressed.connect(self.thread_p2.pause)  # type: ignore
+            self.button_pause.pressed.connect(self.thread_p3.pause)  # type: ignore
+            self.button_pause.pressed.connect(self.thread_p4.pause)  # type: ignore
+
+            self.button_finish.pressed.connect(self.thread_p1.finish)  # type: ignore
+            self.button_finish.pressed.connect(self.thread_p2.finish)  # type: ignore
+            self.button_finish.pressed.connect(self.thread_p3.finish)  # type: ignore
+            self.button_finish.pressed.connect(self.thread_p4.finish)  # type: ignore
+
             self.thread_pool.start(self.thread_p1)
-            self.label_state_p1.setText('En Ejecucion')
 
             # Disable buttons while running
             self.button_assign.setDisabled(True)
             self.button_execute.setDisabled(True)
+            return
+        
+        QMessageBox.warning(self.container, 'Cuidado', 'Asigne procesos primero')
     # start_execution
 
-    ## Finish signals, start the next task after the previous one
+    # Finish signals, start the next task after the previous one
     def finish_p1(self):
         self.thread_pool.start(self.thread_p2)
-        self.label_state_p2.setText('En Ejecucion')
-    
+
     def finish_p2(self):
         self.thread_pool.start(self.thread_p3)
-        self.label_state_p3.setText('En Ejecucion')
-    
+
     def finish_p3(self):
         self.thread_pool.start(self.thread_p4)
-        self.label_state_p4.setText('En Ejecucion')
-    
+
     def finish_p4(self):
         # Make available to run new tasks
         self.button_assign.setDisabled(False)
-    
-    ## Update signals, set the values in the bars
+        QMessageBox.information(self.container, 'Ejecucion Terminada', 'El Lote termino de ejecutarse')
+
+    # Update signals, set the values in the bars
     def update_progress_p1(self, n):
         self.progress_bar_p1.setValue(n)
-    
+
     def update_progress_p2(self, n):
         self.progress_bar_p2.setValue(n)
-    
+
     def update_progress_p3(self, n):
         self.progress_bar_p3.setValue(n)
-    
+
     def update_progress_p4(self, n):
         self.progress_bar_p4.setValue(n)
 
 
 if __name__ == '__main__':
-    ### Start the program
+    # Start the program
     app = QApplication()
     window = FIFOMainWindow()
     window.show()
