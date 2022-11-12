0cc88ca4f029af7143447125bd009515a5b7e2b4
tonomr <tonomac7@gmail.com>
2022-11-12 02:11:33 -0600

    p-c problem ready

--- producer-consumer_problem.py
+++ producer-consumer_problem.py
@@ -1,26 +1,81 @@
-import sys
+""" 
+producer-consumer_problem.py
+Antonio Magaña Reynoso - 218744856
+Seminario de Solucion de Problemas de Sistemas Operativos - D05
+"""
+
+from sys import exit
 from time import sleep
 
 from PySide6.QtCore import (
     QCoreApplication, QMetaObject, Qt, QObject, QRunnable, QThreadPool, Signal, Slot)
 from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout,
-                               QLabel, QMainWindow, QProgressBar, QPushButton, QWidget, QMessageBox)
+                               QLabel, QMainWindow, QProgressBar, QPushButton, QWidget)
 
 
 class SignalProgress(QObject):
     progress = Signal(int)
+    stopped = Signal()
 
 
-class RunProgress(QRunnable):
-    def __init__(self, signal):
+class RunProducer(QRunnable):
+    def __init__(self, signal: SignalProgress, buffer: QProgressBar, status: QLabel):
         super().__init__()
         self.signal = signal
+        self.buffer = buffer
+        self.status = status
+        self.is_stop = False
+
+    @Slot()
+    def run(self) -> None:
+        """ Increase the progress bar value until reach the limit or stopped """
+        self.is_stop = False
+        self.status.setText('Produciendo...')
+
+        while True:
+            actual_value = self.buffer.value()
+            if actual_value >= 100 or self.is_stop:
+                break
+
+            self.signal.progress.emit(actual_value + 10)
+            sleep(1.0)
+
+        self.signal.stopped.emit()
+
+    def stop(self) -> None:
+        """ Stop increasing the progress bar value """
+        self.is_stop = True
+        self.status.setText('Inactivo')
+
+
+class RunConsumer(QRunnable):
+    def __init__(self, signal: SignalProgress, buffer: QProgressBar, status: QLabel):
+        super().__init__()
+        self.signal = signal
+        self.buffer = buffer
+        self.status = status
+        self.is_stop = False
 
     @Slot()
     def run(self):
-        for s in range(10):
-            print(f'Here we are {s}s')
+        """ Decrease the progress bar value until reach the limit or stopped """
+        self.is_stop = False
+        self.status.setText('Consumiendo...')
+
+        while True:
+            actual_value = self.buffer.value()
+            if actual_value <= 0 or self.is_stop:
+                break
+
+            self.signal.progress.emit(actual_value - 10)
             sleep(1.0)
+
+        self.signal.stopped.emit()
+
+    def stop(self) -> None:
+        """ Stop decreasing the progress bar value """
+        self.is_stop = True
+        self.status.setText('Inactivo')
 
 
 class ProducerConsumerWindow(QMainWindow):
@@ -28,7 +83,7 @@ class ProducerConsumerWindow(QMainWindow):
         super().__init__()
         self.setupUi(self)
 
-    def setupUi(self, MainWindow):
+    def setupUi(self, MainWindow: QMainWindow) -> None:
         """ Create the widgets to setup the window """
         # Window configuration
         if not MainWindow.objectName():
@@ -105,9 +160,10 @@ class ProducerConsumerWindow(QMainWindow):
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, MainWindow: QMainWindow) -> None:
+        """ Set text in widgets """
         MainWindow.setWindowTitle(QCoreApplication.translate(
-            "MainWindow", u"Producer-Consumer problem", None))
+            "MainWindow", u"Producer-Consumer problem - AMR", None))
         self.label_consumer.setText(QCoreApplication.translate(
             "MainWindow", u"Proceso Consumidor", None))
         self.label_status.setText(QCoreApplication.translate(
@@ -128,28 +184,60 @@ class ProducerConsumerWindow(QMainWindow):
             QCoreApplication.translate("MainWindow", u"Detener", None))
     # retranslateUi
 
-    def run_producer(self):
-        print("Running producer!")
-        self.s1 = SignalProgress()
-        self.t1 = RunProgress(self.s1)
-        self.thread_pool.start(self.t1)
+    def run_producer(self) -> None:
+        """ Increase progress bar value with threads """
+        self.pushButton_producer.setDisabled(True)
+        self.pushButton_consumer.setDisabled(True)
+        self.pushButton_stop_consumer.setDisabled(True)
+
+        self.signal_producer = SignalProgress()
+        self.producer = RunProducer(
+            self.signal_producer, self.progressBar_buffer, self.label_status)
+        self.producer.signal.progress.connect(self.update_progress)
+        self.producer.signal.stopped.connect(self.stop_producer)
+
+        self.thread_pool.start(self.producer)
+    # run_producer
+
+    def run_consumer(self) -> None:
+        """ Decrease progress bar value with threads """
+        self.pushButton_producer.setDisabled(True)
+        self.pushButton_consumer.setDisabled(True)
+        self.pushButton_stop_producer.setDisabled(True)
+
+        self.signal_consumer = SignalProgress()
+        self.consumer = RunConsumer(
+            self.signal_consumer, self.progressBar_buffer, self.label_status)
+        self.consumer.signal.progress.connect(self.update_progress)
+        self.consumer.signal.stopped.connect(self.stop_consumer)
+
+        self.thread_pool.start(self.consumer)
     # run_consumer
 
-    def stop_producer(self):
-        print("Stopping producer!")
+    def stop_producer(self) -> None:
+        """ Stop increasing value """
+        self.producer.stop()
+        self.pushButton_producer.setDisabled(False)
+        self.pushButton_consumer.setDisabled(False)
+        self.pushButton_stop_consumer.setDisabled(False)
     # stop_producer
 
-    def run_consumer(self):
-        print("Running consumer!")
-    # run_consumer
-
-    def stop_consumer(self):
-        print("Stopping consumer!")
+    def stop_consumer(self) -> None:
+        """ Stop decreasing value """
+        self.consumer.stop()
+        self.pushButton_producer.setDisabled(False)
+        self.pushButton_consumer.setDisabled(False)
+        self.pushButton_stop_producer.setDisabled(False)
     # stop_consumer
+
+    def update_progress(self, n: int) -> None:
+        """ Update the progress value """
+        self.progressBar_buffer.setValue(n)
+    # update_progress
 
 
 if __name__ == "__main__":
     app = QApplication([])
     window = ProducerConsumerWindow()
     window.show()
-    sys.exit(app.exec())
+    exit(app.exec())
